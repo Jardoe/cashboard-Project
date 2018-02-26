5590970fbd13a16e1e1ce564506562d3973dc1e5
user <user@users-MacBook-Pro.local>
2018-02-26 12:08:35 +0000

    Added edit functionality

--- controllers/transactions_controller.rb
+++ controllers/transactions_controller.rb
@@ -4,13 +4,39 @@ require_relative( '../models/transaction.rb' )
 require_relative('../models/tag.rb')
 require_relative('../models/merchant.rb')
 
-get '/transactions' do
-  @transaction = Transaction.find(params['id'].to_i)
-  erb(:"transaction/show")
+# get '/transactions' do
+#   @transaction = Transaction.find(params['id'].to_i)
+#   erb(:"transaction/show")
+# end
+
+get '/new' do
+  @merchants = Merchant.all()
+  @tags = Tag.all()
+  erb(:"transactions/new")
 end
 
-post '/new' do
+post '/transactions/added' do
   transaction = Transaction.new(params)
   transaction.save
-  redirect to (:"transactions/new")
+  redirect to (:"transactions/added")
+end
+
+get '/transactions/added' do
+  erb(:"transactions/added")
+end
+
+get '/transactions/:id/edit' do
+  @merchants = Merchant.all()
+  @tags = Tag.all()
+  @transaction = Transaction.find(params[:id])
+  erb( :"transactions/edit" )
+end
+
+get '/transactions/:id/view' do
+  erb(:"transactions/show")
+end
+
+post '/tranasctions/:id/delete' do
+  Transaction.delete(params[:id])
+  redirect to (:"transactions/delete")
 end
--- models/transaction.rb
+++ models/transaction.rb
@@ -6,8 +6,8 @@ class Transaction
 
   def initialize(options)
     @id = options['id'].to_i if options['id']
-    @merchant_id = options['merchant_id']
-    @tag_id = options['tag_id']
+    @merchant_id = options['merchant_id'].to_i
+    @tag_id = options['tag_id'].to_i
     @amount = options['amount'].to_f
   end
 
